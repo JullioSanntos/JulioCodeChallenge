340547899f8fd06a35a670ca8007b3244ea6f9c2
JullioSanntos <JulioSantos@Hotmail.com>
2025-04-21 14:45:29 -0400

    Service beiong loaded in a grid

--- JulioCode.ViewModels/MainViewModel.cs
+++ JulioCode.ViewModels/MainViewModel.cs
@@ -4,13 +4,16 @@ using JulioCode12.Common;
 using JulioCode12.Common.WPF;
 
 namespace JulioCode06.ViewModels; 
-public class MainViewModel  {
+public class MainViewModel : SetPropertyBase  {
     public ICommand LoadTradeCommand => new RelayCommand(async _ => await LoadTrades());
 
-    #region List<trade>
-    public List<Trade>? TradesList { get; set; }
-
-    #endregion List<trade>
+    #region TradesList
+    private List<Trade>? _tradesList;
+    public List<Trade> TradesList {
+        get { return _tradesList ??= []; }
+        set => SetProperty(ref _tradesList, value);
+    }
+    #endregion TradesList
 
     public async Task LoadTrades() {
         var loadServices = new LoadTradesService();
